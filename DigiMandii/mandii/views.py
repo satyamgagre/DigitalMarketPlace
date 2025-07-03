9c2c60df0bc5be8ac8f2fa6e5ba1124a3cfa7de8
Satyam Gagare <satyamgagre4@gmail.com>
2025-07-04 01:35:56 +0530

    04-07-2025

--- DigiMandii/mandii/views.py
+++ DigiMandii/mandii/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'mandii/index.html')
+    products = Product.objects.all()
+    return render(request, 'mandii/index.html',{'products':products})
+
+def detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'mandii/detail.html',{'product':product})
